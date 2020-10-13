504418f7a75507980d01180474b1562f80b619dc
leon123858 <a0970785699@gmail.com>
2020-10-13 19:34:10 +0800

    增加QQ/EQ 功能

--- PQ_main/PQ_main/routes/GetConst.js
+++ PQ_main/PQ_main/routes/GetConst.js
@@ -3,6 +3,8 @@ function Get(thing) {
     switch (thing) {
         case "mongoPath":
             return 'mongodb://localhost:27017/';
+        case "QQ/EQ":
+            return 'https://leon123858.github.io/questionareSystem/UCS/confirm.html';
         default:
             return '';
     }
--- PQ_main/PQ_main/routes/index.js
+++ PQ_main/PQ_main/routes/index.js
@@ -1,10 +1,16 @@
 ﻿'use strict';
 var express = require('express');
+const { Get } = require('./GetConst');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('EW/EW1');
+});
+
+//QQ/EQ
+router.post('/QQ/EQ', function (req, res) {
+    res.render('goto', { url: Get('QQ/EQ') });
 });
 
 module.exports = router;
