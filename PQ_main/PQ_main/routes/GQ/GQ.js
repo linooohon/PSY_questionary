ae4b141321b3bf25bb3e01a7ad9dea5908f6b8a0
leon123858 <a0970785699@gmail.com>
2020-10-17 17:34:58 +0800

    EQ前端GUI 完成

--- PQ_main/PQ_main/routes/GQ/GQ.js
+++ PQ_main/PQ_main/routes/GQ/GQ.js
@@ -11,9 +11,16 @@ router.use('/SQ', SQ);
 /**********************
  ./GQ/EQ
  ***********************/
-
 router.post('/EQ', function (req, res) {
     res.render('GQ/EQ', { ID: req.body.ID, password: req.body.password });
+});
+
+/**********************
+./GQ/Questionary/: which
+ ***********************/
+
+router.post('/Questionary/:which', function (req, res) {
+    res.render('GQ/questionary/' + req.params.which, { ID: req.body.ID, password: req.body.password });
 });
 
 module.exports = router;
