2fffb303f8e0d5b547a47ccdf70cee4d0a34742a
leon123858 <a0970785699@gmail.com>
2020-10-25 14:18:11 +0800

    新增 上次填寫時間 功能

--- PQ_main/PQ_main/routes/GQ/SQ.js
+++ PQ_main/PQ_main/routes/GQ/SQ.js
@@ -13,29 +13,29 @@ const namesList_one = {
     A: ['color', 'Acc', 'RT'],
     B: ['color', 'CorrAns', 'Press', 'Acc', 'RT', 'SSD', 'SS_Acc'],
     C: ['CoherenceRate', 'RT', 'Acc', 'Direction', 'PressKey'],
-    D: ['Size', 'PresentTime', 'Direction','Press','Acc'],
-    E: ['Cue', 'Congruent', 'Position', 'Orientation','Press','Acc','RT'],
-    F: ['Cue','SoA','TargetPosition','Press','Acc'],
+    D: ['Size', 'PresentTime', 'Direction', 'Press', 'Acc'],
+    E: ['Cue', 'Congruent', 'Position', 'Orientation', 'Press', 'Acc', 'RT'],
+    F: ['Cue', 'SoA', 'TargetPosition', 'Press', 'Acc'],
     G: ['Point'],
     H: ['Color', 'CorrAns', 'Press', 'Acc', 'RT'],
     I: ['NofDig', 'CorrDig', 'PressDig', 'Accuracy', 'Sum', 'RT'],
-    J: ['Position','CorrAns','Press','Accuracy'],
+    J: ['Position', 'CorrAns', 'Press', 'Accuracy'],
     K: ['Condition', 'Pic', 'Color', 'Press', 'Acc', 'RT'],
-    L: ['filename','X','Y','Sub_X','Sub_Y','Spin','Ans','Acc'],
-    M: ['file','CorrAns','Ans','Acc']
+    L: ['filename', 'X', 'Y', 'Sub_X', 'Sub_Y', 'Spin', 'Ans', 'Acc'],
+    M: ['file', 'CorrAns', 'Ans', 'Acc']
 }
 
 const namesList_group = {
     A: ['ACC', 'RT', 'FA', 'FA_RT'],
     B: ['ACC', 'Go_Acc', 'Go_RT', 'NCRate', 'NC_RT', 'mSSD', 'SSRT'],
     C: ['ACC_R1', 'ACC_R2', 'ACC_R3', 'CR_R1', 'CR_R2', 'CR_R3', 'AVR_R'],
-    D: ['R1t1', 'R1t3', 'R1t10', 'R1Acc1', 'R1Acc3', 'R1Acc10', 'R3t1', 'R3t3', 'R3t10', 'R3Acc1', 'R3Acc3', 'R3Acc10', 'R10t1', 'R10t3', 'R10t10', 'R10Acc1', 'R10Acc3', 'R10Acc10','mt1','mt3','mt10','SI'],
-    E: ['Acc','RT','NoCue','Center','Dual','Spatial','Cong','Ing','Neutral','Alert','Orientation','Conflict'],
-    F: ['Acc','RT','Neutral','Congruent','InCongruent','SoA200','SoA1200'],
+    D: ['R1t1', 'R1t3', 'R1t10', 'R1Acc1', 'R1Acc3', 'R1Acc10', 'R3t1', 'R3t3', 'R3t10', 'R3Acc1', 'R3Acc3', 'R3Acc10', 'R10t1', 'R10t3', 'R10t10', 'R10Acc1', 'R10Acc3', 'R10Acc10', 'mt1', 'mt3', 'mt10', 'SI'],
+    E: ['Acc', 'RT', 'NoCue', 'Center', 'Dual', 'Spatial', 'Cong', 'Ing', 'Neutral', 'Alert', 'Orientation', 'Conflict'],
+    F: ['Acc', 'RT', 'Neutral', 'Congruent', 'InCongruent', 'SoA200', 'SoA1200'],
     G: ['Level', 'Score'],
     H: ['Acc1', 'Acc2', 'Acc3', 'RT1', 'RT2', 'RT3'],
     I: ['Score', 'Acc'],
-    J: ['N','Acc','Score','back_Score2','back_Score3'],
+    J: ['N', 'Acc', 'Score', 'back_Score2', 'back_Score3'],
     K: ['Acc', 'RT', 'Positive', 'Negative', 'Middle'],
     L: [],
     M: []
@@ -147,10 +147,22 @@ function saveInDB(type, mode, str) {
     }
 }
 
-function insertData(db,ID,type,date,mode,data) {
+function insertData(db, ID, type, date, mode, data) {
     return new Promise((resolve, reject) => {
         var table = db.db("GQ_data").collection(type + "_" + mode);
-        table.insertOne({ ID: ID, Date: date, data: saveInDB(type,mode,data) }, function (err, result) {
+        table.insertOne({ ID: ID, Date: date, data: saveInDB(type, mode, data) }, function (err, result) {
+            if (err) { reject({ result: '伺服器連線錯誤' }); throw err; }
+            resolve({ result: "success" });
+        });
+    });
+}
+
+function updateDate(db, ID, date, type) {
+    return new Promise((resolve, reject) => {
+        var table = db.db("GQ_personal").collection("personal_Date");
+        var updateThing = {};
+        updateThing[type] = date;
+        table.updateOne({ ID: ID }, { $set: updateThing }, { upsert: true }, function (err, result) {
             if (err) { reject({ result: '伺服器連線錯誤' }); throw err; }
             resolve({ result: "success" });
         });
@@ -163,7 +175,7 @@ router.post('/saveData', function (req, res) {
     var one = req.body.one;//string
     var group = req.body.group;//string
     var type = req.body.type;
-    var date = new Date();
+    var date = new Date().toLocaleString();
     //console.log(req.body);
     MongoClient.connect(Get("mongoPath") + 'EW', { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
         if (err) { res.json({ result: '伺服器連線錯誤' }); throw err; }
@@ -173,7 +185,8 @@ router.post('/saveData', function (req, res) {
         CheckPasswordAndLicence(db, ID, password, type)
             .then(pkg => insertData(db, ID, type, date, "one", one))
             .then(pkg => insertData(db, ID, type, date, "group", group))
-            .then(pkg => res.json({result:"success"}))
+            .then(pkg => updateDate(db, ID, date, type))
+            .then(pkg => res.json({ result: "success" }))
             .catch(error => res.json(error));
     });
 });
@@ -192,14 +205,14 @@ function CheckPassword(db, ID, password) {
                 reject({ result: '不存在此帳號' });
             else
                 if (result.password == password)
-                        resolve(1);
+                    resolve(1);
                 else
                     reject({ result: "密碼錯誤" });
         });
     });
 }
 
-function CheckIfHave(db,pathname) {
+function CheckIfHave(db, pathname) {
     return new Promise((resolve, reject) => {
         var table = db.db("GQ_data").collection("M_video");
         table.findOne({ pathname: pathname }, { projection: { _id: 0 } }, function (err, result) {
@@ -215,7 +228,7 @@ function CheckIfHave(db,pathname) {
 function InsertVideo(db, pathname) {
     return new Promise((resolve, reject) => {
         var table = db.db("GQ_data").collection("M_video");
-        table.insertOne({ pathname: pathname, Corr: 0, InCorr:0 }, function (err, result) {
+        table.insertOne({ pathname: pathname, Corr: 0, InCorr: 0 }, function (err, result) {
             if (err) { reject({ result: '伺服器連線錯誤' }); throw err; }
             resolve(1);
         });
@@ -227,7 +240,7 @@ function updateVideo(db, pathname, ans) {
         var table = db.db("GQ_data").collection("M_video");
         var Addthing = {};
         Addthing[ans] = 1;
-        table.updateOne({ pathname: pathname }, { $inc: Addthing } , function (err, result) {
+        table.updateOne({ pathname: pathname }, { $inc: Addthing }, function (err, result) {
             if (err) { reject({ result: '伺服器連線錯誤' }); throw err; }
             resolve({ result: 'success' });
         });
@@ -245,10 +258,10 @@ router.post('/videoResult', function (req, res) {
             if (err) { res.json({ result: '伺服器連線錯誤' }); throw err; }
             CheckPassword(db, ID, password)
                 .then(pkg => CheckIfHave(db, pathname)).then(pkg => {
-                if (pkg)
-                    InsertVideo(db, pathname).then(pkg => updateVideo(db, pathname, ans));
-                else
-                    updateVideo(db, pathname, ans);
+                    if (pkg)
+                        InsertVideo(db, pathname).then(pkg => updateVideo(db, pathname, ans));
+                    else
+                        updateVideo(db, pathname, ans);
                 })
                 .then(pkg => res.json(pkg))
                 .catch(error => res.json(error));
