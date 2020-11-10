8b56291e24aed3adf97cb0424b2073a3f4a54934
leon123858 <a0970785699@gmail.com>
2020-11-10 15:58:58 +0800

    試畫D問卷響應時間
    Signed-off-by: leon123858 <a0970785699@gmail.com>

---
+++ DrawPQ/DrawPQ/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DrawPQ
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void getPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "csv files (*.csv)|*.csv";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = dialog.FileName;
+            }
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            chart.Series.Clear();
+            try
+            {
+                DataHolder dataHolder = new DataHolder(path.Text);
+                for (int i = 1; i <= dataHolder.length; i++)
+                {
+                    var data = dataHolder.Draw(comboBox.Text, i);
+                    Series series1 = new Series("第" + i + "條線", 1000);
+                    series1.ChartType = SeriesChartType.Line;
+                    //將數值新增至序列
+                    for (int index = 0; index < data.Count; index++)
+                    {
+                        series1.Points.AddXY(index, data[index]);
+                    }
+                    chart.Series.Add(series1);
+                }
+                chart.Titles.Clear();
+                chart.Titles.Add("問卷" + comboBox.Text);
+            }
+            catch (Exception err){
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
