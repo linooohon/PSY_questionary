5ed4de348976ee3b873106d5b90f350db37afcf0
leon123858 <56253942+leon123858@users.noreply.github.com>
2021-04-18 13:53:27 +0800

    addJsPsychAndWhichSample
    addJsPsychAndWhichSample

--- PQ_MDB/PQ_MDB/routes/GetConst.js
+++ PQ_MDB/PQ_MDB/routes/GetConst.js
@@ -1,17 +1,18 @@
 ﻿require('dotenv').config();
 
 function Get(thing) {
-    switch (thing) {
-        case "mongoPath":
-            return 'mongodb://localhost:27017/';
-        case 'ID':
-            return process.env.ID;
-        case 'password':
-            return process.env.password;
-        default:
-            return '';
-    }
+	switch (thing) {
+		// case "mongoPath":
+		//     return 'mongodb://localhost:27017/';
+		case 'mongoPath':
+			return process.env.mongoPath;
+		case 'ID':
+			return process.env.ID;
+		case 'password':
+			return process.env.password;
+		default:
+			return '';
+	}
 }
-
 
 module.exports.Get = Get;
