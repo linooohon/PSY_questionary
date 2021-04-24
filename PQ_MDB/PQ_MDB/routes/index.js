10c4b0240df6685bff22c94c37902ca388db929c
leon123858 <56253942+leon123858@users.noreply.github.com>
2021-04-25 00:51:40 +0800

    MDB add CQ
    MDB add CQ

--- PQ_MDB/PQ_MDB/routes/index.js
+++ PQ_MDB/PQ_MDB/routes/index.js
@@ -10,7 +10,7 @@ const core_password = Get('password');
  ./
   **************************************/
 router.get('/', function (req, res) {
-    res.render('signPage');
+	res.render('signPage');
 });
 
 /**************************************
@@ -18,47 +18,54 @@ router.get('/', function (req, res) {
   **************************************/
 
 router.post('/login', function (req, res) {
-    var ID = req.body.ID;
-    var password = req.body.password
-    if (core_ID == ID && core_password == password)
-        res.render('switchPage', { ID: ID, password: password })
-    else
-        res.render('warming', { message: "帳號或密碼錯誤" });
+	var ID = req.body.ID;
+	var password = req.body.password;
+	if (core_ID == ID && core_password == password)
+		res.render('switchPage', { ID: ID, password: password });
+	else res.render('warming', { message: '帳號或密碼錯誤' });
 });
 
 /**************************************
  ./base
   **************************************/
 router.post('/base', function (req, res) {
-    var ID = req.body.ID;
-    var password = req.body.password
-    if (core_ID == ID && core_password == password)
-        res.render('basePage', { ID: ID, password: password })
-    else
-        res.render('warming', { message: "帳號或密碼錯誤" });
+	var ID = req.body.ID;
+	var password = req.body.password;
+	if (core_ID == ID && core_password == password)
+		res.render('basePage', { ID: ID, password: password });
+	else res.render('warming', { message: '帳號或密碼錯誤' });
 });
 /**************************************
  ./GQ
   **************************************/
 router.post('/GQ', function (req, res) {
-    var ID = req.body.ID;
-    var password = req.body.password
-    if (core_ID == ID && core_password == password)
-        res.render('GQPage', { ID: ID, password: password })
-    else
-        res.render('warming', { message: "帳號或密碼錯誤" });
+	var ID = req.body.ID;
+	var password = req.body.password;
+	if (core_ID == ID && core_password == password)
+		res.render('GQPage', { ID: ID, password: password });
+	else res.render('warming', { message: '帳號或密碼錯誤' });
 });
 
 /**************************************
  ./QQ
   **************************************/
 router.post('/QQ', function (req, res) {
-    var ID = req.body.ID;
-    var password = req.body.password
-    if (core_ID == ID && core_password == password)
-        res.render('QQPage', { ID: ID, password: password })
-    else
-        res.render('warming', { message: "帳號或密碼錯誤" });
+	var ID = req.body.ID;
+	var password = req.body.password;
+	if (core_ID == ID && core_password == password)
+		res.render('QQPage', { ID: ID, password: password });
+	else res.render('warming', { message: '帳號或密碼錯誤' });
+});
+
+/**************************************
+ ./CQ
+  **************************************/
+router.post('/CQ', function (req, res) {
+	var ID = req.body.ID;
+	var password = req.body.password;
+	if (core_ID == ID && core_password == password)
+		res.render('CQPage', { ID: ID, password: password });
+	else res.render('warming', { message: '帳號或密碼錯誤' });
 });
 
 module.exports = router;
