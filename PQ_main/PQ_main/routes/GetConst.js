e3ba6ea38b615b8d96abc2c8620276978cfbb41c
leon123858 <56253942+leon123858@users.noreply.github.com>
2021-04-25 21:02:04 +0800

    信箱註冊功能
    新增 信箱註冊功能

--- PQ_main/PQ_main/routes/GetConst.js
+++ PQ_main/PQ_main/routes/GetConst.js
@@ -10,6 +10,14 @@ function Get(thing) {
 		//    return 'https://leon123858.github.io/questionareSystem/UCS/confirm.html';
 		case 'SRSurl':
 			return process.env.SRSurl;
+		case 'Gmail_ID':
+			return process.env.Gmail_ID;
+		case 'Gmail_password':
+			return process.env.Gmail_password;
+		case 'sendMailer':
+			return `使用者註冊信 <${process.env.Gmail_ID}>`;
+		case 'local_uri':
+			return process.env.local_uri;
 		default:
 			return '';
 	}
